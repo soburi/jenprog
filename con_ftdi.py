e761a3e08707a528690226cd7c92a7c4629c82e5
soburi <soburi@gmail.com>
2018-06-03 01:07:37 +0900

    Use libftdi1 python bindings

--- con_ftdi.py
+++ con_ftdi.py
@@ -31,10 +31,11 @@
 # Author(s): Philipp Scholl <scholl@teco.edu>
 
 
-from ftdi import *
+import ftdi1
 from time import sleep
 from flashutils import JennicProtocol
 import logging
+import usbutils
 
 class Closure:
     def __init__(self, arg, func):
@@ -42,40 +43,47 @@ class Closure:
         self.func = func
 
     def __call__(self, *args):
-        val = self.func(self.arg, *args)
-        if val < 0:
-            raise Exception("%s: %d"%(ftdi_get_error_string(self.arg), val))
+        ret = self.func(self.arg, *args)
+        if isinstance(ret, list):
+            val = ret[0]
         else:
-            return val
+            val = ret
+
+        if val < 0:
+            raise Exception("%s: %d"%(ftdi1.ftdi_get_error_string(self.arg), val))
+        else:
+            return ret
 
 class Ftdi:
     def __init__(self):
-        self.context = ftdi_context()
-        ftdi_init(self.context)
+        self.context = ftdi1.new()
+
+    def __del__(self):
+        ftdi1.free(self.context)
 
     def __call__(self, *args):
         print(args)
 
     def __getattr__(self, name):
-        return Closure(self.context, eval("ftdi_%s"%name))
+        return Closure(self.context, eval("ftdi1.%s"%name))
 
 class FtdiBootloader(JennicProtocol):
     # use bitbang mode to jump into programming mode, see
     # enterprogrammingmode
     def __init__(self, vid='0x0403', pid='0x6001', device=None):
         self.f = Ftdi()
-        self.NONE = 0x00
         self.VID, self.PID = int(vid, 16), int(pid, 16)
+        self.SERIAL = usbutils.query_iserial(device)
 
-        self.f.usb_open_desc(self.VID, self.PID, None, device)
+        self.f.usb_open_desc(self.VID, self.PID, None, self.SERIAL)
 
         self.enterprogrammingmode()
         self.doreset = 1
 
         self.f.set_baudrate(38400)
-        self.f.set_line_property(NONE, STOP_BIT_1, BITS_8)
+        self.f.set_line_property(ftdi1.BITS_8, ftdi1.STOP_BIT_1, ftdi1.NONE)
         self.f.setrts(1)
-        self.f.setflowctrl(SIO_RTS_CTS_HS)
+        self.f.setflowctrl(ftdi1.SIO_RTS_CTS_HS)
         self.f.setrts(0)
 
         self.f.usb_purge_buffers()
@@ -92,11 +100,11 @@ class FtdiBootloader(JennicProtocol):
         CBUS3 is connected to SPIMISO
         CBUS2 is connected to RESET
         """
-        self.f.set_bitmode(0xF3, BITMODE_CBUS);
+        self.f.set_bitmode(0xF3, ftdi1.BITMODE_CBUS);
         sleep(.05)
-        self.f.set_bitmode(0xF7, BITMODE_CBUS);
+        self.f.set_bitmode(0xF7, ftdi1.BITMODE_CBUS);
         sleep(.05)
-        self.f.set_bitmode(0xFF, BITMODE_CBUS);
+        self.f.set_bitmode(0xFF, ftdi1.BITMODE_CBUS);
         sleep(.05)
         self.f.disable_bitbang()
         sleep(.05)
@@ -124,7 +132,7 @@ class FtdiBootloader(JennicProtocol):
         assert msg_len < 0xFF, "oversized msg, max is 256 bytes, yours is %i"%msg_len
 
         # pack optional args in
-        msg    = cArray(msg_len)
+        msg    = [0 for i in range(msg_len) ]
         msg[0] = msg_len-1
         msg[1] = msg_type
         i      = 2
@@ -148,7 +156,7 @@ class FtdiBootloader(JennicProtocol):
                 i     += 1
 
         # add crc
-        msg[i] = self.crc(msg, msg_len-1)
+        msg[i] = self.crc(msg, msg_len-1).to_bytes(1, byteorder='little')[0]
         assert msg[i] == self.crc(msg, msg_len-1), "%i != %i"%(msg[i], self.crc(msg, msg_len))
 
         str = ""
@@ -160,25 +168,23 @@ class FtdiBootloader(JennicProtocol):
         # construct answer storage
         if ans_type != None:
             ans_len = 0
-            ans     = cArray(1)
 
             # send the message until there is an answer
             while ans_len == 0:
                 waited = 0
-                self.f.write_data(msg, msg_len)
+                self.f.write_data(bytes(msg))
 
                 # wait some cycles until the message will be repeated
                 while waited < 150 and ans_len == 0:
-                    ans_len = self.f.read_data(ans, 1)
+                    ans_len, ans = self.f.read_data(1)
                     waited  += 1
 
             # okay we received the first byte of the answer,
             # which contains the length of the answer
             ans_len = ans[0]
-            ans     = cArray(ans_len)
 
             # now read the answer
-            read_len = self.f.read_data(ans, ans_len)
+            read_len, ans = self.f.read_data(ans_len)
             assert read_len == ans_len, "%i != %i" % (read_len, ans_len)
             assert ans[0] == ans_type, "recvd: 0x%x, anticipiated: 0x%x" %( ans[0], ans_type )
 
@@ -196,11 +202,12 @@ class FtdiBootloader(JennicProtocol):
         Switch to bitbang and toggle reset line.
         """
         if self.doreset:
-            self.f.set_bitmode(0xFB, BITMODE_CBUS);
+            self.f.set_bitmode(0xFB, ftdi1.BITMODE_CBUS);
             sleep(.1)
-            self.f.set_bitmode(0xFF, BITMODE_CBUS);
+            self.f.set_bitmode(0xFF, ftdi1.BITMODE_CBUS);
             sleep(.1)
             self.f.disable_bitbang()
 
         self.f.usb_close()
+        usbutils.driver_restore(self.VID, self.PID, self.SERIAL)
 
---
+++ usbutils.py
@@ -0,0 +1,70 @@
+import os
+import re
+import sys
+
+def query_iserial_linux(devname):
+    devbody = re.sub(r'^.*\/', '', devname)
+    devfile = '/sys/bus/usb-serial/devices/' + devbody
+    result = os.readlink(devfile)
+
+    if result[0] != '/':
+        serialinfo = os.path.join(os.path.dirname(devfile), result, '../../serial')
+    else:
+        serialinfo = result
+
+    f = open(serialinfo)
+    if f != None:
+        iserial = f.read().strip()
+        f.close()
+        return iserial
+
+    raise Exception(devname + 'Not Found')
+
+def query_iserial_windows(devname):
+    import wmi
+    c  = wmi.WMI(namespace='CIMV2')
+    result = c.query('SELECT * FROM Win32_PnPEntity WHERE ClassGuid="{4d36e978-e325-11ce-bfc1-08002be10318}"')
+
+    for pnp in result:
+        compatt = re.compile(r'.*\((COM\d+)\).*')
+        matched = re.match(compatt, pnp.Caption)
+        comstr = matched.group(1)
+
+        serpatt = re.compile(r'^FTDIBUS\\VID_\d{4}\+PID_\d{4}\+(\w{8}).*')
+        matched = re.match(serpatt, pnp.DeviceID)
+        iserial = matched.group(1)
+
+        if comstr == devname:
+            return iserial
+
+    raise Exception(devname + 'Not Found')
+
+def driver_restore_linux(vid, pid, serial):
+    import usb1
+    with usb1.USBContext() as context:
+        for device in context.getDeviceIterator(skip_on_error=True):
+            if device.getVendorID() == vid and device.getProductID() == pid:
+                handle = device.open()
+                for configuration in device.iterConfigurations():
+                    for interface in configuration:
+                        for setting in interface:
+                            if not handle.kernelDriverActive(setting.getNumber()):
+                                handle.attachKernelDriver(setting.getNumber())
+
+def driver_restore_windows(vid, pid, serial):
+    return
+
+def query_iserial(devname):
+    try:
+        func = eval('query_iserial_' + sys.platform)
+    except Exception as e:
+        print(e)
+        raise Exception('Not supported platform')
+    return func(devname)
+
+def driver_restore(vid, pid, serial):
+    try:
+        func = eval('driver_restore_' + sys.platform)
+    except:
+        raise Exception('Not supported platform')
+    func(vid, pid, serial)
